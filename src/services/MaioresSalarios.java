c7bb7c5ab8e41557d2058afc2a72df5ae70c9379
MatheusCaa <matheuusamararal@gmail.com>
2024-08-08 11:12:47 -0300

    10 pessoas maiores salários

---
+++ src/services/MaioresSalarios.java
@@ -0,0 +1,27 @@
+package services;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import model.Funcionario;
+
+public class MaioresSalarios {
+
+	// 1 - Pegar os lista de 10 funcionarios com maior salário
+
+	public static void main(String[] args) {
+	}
+
+	public static List<Funcionario> filtraPorSalario(List<Funcionario> funcionarios) {
+
+		List<Funcionario> funcionariosFiltrados = new ArrayList<Funcionario>();
+
+		return funcionariosFiltrados = funcionarios.stream()
+				.sorted(Comparator.comparing(Funcionario::getSalario).reversed())
+				.limit(10)
+				.collect(Collectors.toCollection(ArrayList::new));
+	}
+
+}

606a193feb623139e0fa4a9b3ceb570ed12575b8
MatheusKpv <matheusmatias1910@gmail.com>
2024-08-08 11:36:34 -0300

    configurando

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,7 +1,14 @@
 package principal;
 
+import java.util.Comparator;
+import java.util.List;
+
+import model.Funcionario;
+import services.CargaDados;
+
 public class Principal {
 	public static void main(String[] args) {
-		
+		List<Funcionario> ads = new CargaDados().getListaFuncionarios();
+		ads.stream().map(e -> e.getSalario().doubleValue()).forEach(System.out::println);
 	}
 }
